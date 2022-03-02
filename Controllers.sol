d5ad564dbb3f8b4a4c85926218eaecd470316f85
Arjun Jagadeesh <jagadeesh.vabbina@gmail.com>
2022-03-02 07:53:54 +0530

    Controllers in Solidity

---
+++ Controllers.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+contract myWeb3 {
+    function Satoshi() internal {
+        if (msg.value == 100) {   //We can use && || ! for respective uses in conditions
+            //execute some code here 
+        }
+    }
+}
+
+contract Nakamoto {
+    function myBitcoin() external pure {
+        for (uint i=0; i<9; i++) {  //initialise ,stopping condition and increment or decrement 
+            i=0;
+        }
+    }
+}
+
+contract Vitalik {
+    function myEtherum() public pure {
+        bool Buterin = true;
+        while (Buterin != false ) {
+            //do this 
+            if (Buterin = true ) {
+                Buterin = false;
+            }
+        }
+    }
+}
