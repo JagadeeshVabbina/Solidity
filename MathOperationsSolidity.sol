d7f35d90d62ae7f7534da2b4984a0894732e01ce
Arjun.Jagadeesh <jagadeesh.vabbina@gmail.com>
2022-05-07 07:45:56 +0530

    Math operations in Solidity

---
+++ MathOperationsSolidity.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.6.0;
+contract Crowdfund {
+    //Math operations in solidity is straightforward as is in many languages
+    uint camera = 1000 + 500; //Addition
+    uint food = 5000 - 250; //
+    uint assistantD = 7000 * 5;
+    uint ProductionD = 100000/2;
+    uint actors = 25 % 2; //Modulus is 1
+    uint Music = 100 ** 2; //equal to 100^2=10000;
+    }
