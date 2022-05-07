b59c5fec720568a7d6ae20c73f529c8e48857d8c
Arjun.Jagadeesh <jagadeesh.vabbina@gmail.com>
2022-05-07 08:08:39 +0530

    Structs in Solidity

---
+++ struct.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.5.0 <0.6.0;
+contract myMovie {
+    uint budget;
+    /* struct is used for more complex data structure 
+    where we have use for multiple properties 
+    struct {
+        property1
+        property2 
+        } */
+    struct person {
+        string name;
+        uint age;
+    }
+}
+
