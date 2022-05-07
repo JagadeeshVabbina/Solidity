a6c51094be2856ee3e3d847cc64e8b9c62f614f8
Arjun.Jagadeesh <jagadeesh.vabbina@gmail.com>
2022-05-07 09:57:41 +0530

    Function Declarations, parameters and arguments

---
+++ FunctionDeclaration.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.5.0 <0.6.0;
+contract myNFT {
+    function Photography(string memory _indiaunexplored,uint year) public {
+        /* here we have declared a function named Photograghy with the keyword function and it has 
+        two parameters(string & uint) ,in general the parameter variables will be created starting with
+        underscore as a convention.to call a function there are two ways one by value and other by reference.
+        1.by value --> compiler will create a new copy of the original parameters and will assign a new value
+        without changing the original parameter's value.
+        2.by reference --> the function will take the argument's value and pass it to parameters values 
+        thus by making it permanent. 
+        */
+    }
+}
+
