378f0943c58ce54d32e195e57c3eca98b9c5fb06
Arjun.Jagadeesh <jagadeesh.vabbina@gmail.com>
2022-05-27 07:35:17 +0530

    Data types in solidity

---
+++ datatypesinSol.sol
@@ -0,0 +1,20 @@
+//SPDX-Licens-Identifier: MIT
+/* Solidity is a statistically typed language which means , the varibales types will be read at
+compile stage.
+
+They are two types of variables
+1) Value types
+2)Reference Types
+
+1) Value types: directly hold data in storage/memory.
+can store up to 32 bytes(256 bits)
+
+Ex:- int,uint,bool,address,bytes,enum
+
+
+2) reference types:- holds the location where data resides.
+can store more thsn 32 bytes(256 bits)
+
+Ex:- Strings,Arrays,Structs and Mappings.
+
+*/
