4f6a7b436b8c59438ff1218677c49b1b2c970f89
Arjun Jagadeesh <jagadeesh.vabbina@gmail.com>
2022-07-04 06:30:11 +0530

    New sol

---
+++ New.sol
@@ -0,0 +1,6 @@
+//SPDX-License-Identifier: MIT 
+//If there is no license provided , then compiler will give warning errors
+pragma solidity 0.8.7;
+contract HelloWorld{
+    string public myString = "hello world"; //can be read from public
+}
