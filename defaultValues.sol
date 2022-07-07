78d6a33146332bb48ca20deb250ca45316c664f6
Arjun Jagadeesh <jagadeesh.vabbina@gmail.com>
2022-07-07 06:29:58 +0530

    default vaules

---
+++ defaultValues.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.7;
+contract defaultvalues {
+    bool public b; //false
+    uint public u; //0
+    int public i; //0
+    address public a; // 0x0000000000000000000000000000000000000000
+    bytes32 public b32; // 0x0000000000000000000000000000000000000000000000000000000000000000
+}
