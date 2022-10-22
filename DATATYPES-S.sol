8a09515f19cf619ead73b44db9d0bde6096229a1
JagadeeshVabbina <jagadeesh.vabbina@gmail.com>
2022-10-22 13:35:05 +0530

    Data types in solidity

---
+++ DATATYPES-S.sol
@@ -0,0 +1,14 @@
+
+/* Data Types :- 1. Value type 
+                 2. Reference type
+
+1.Value type:- Holds value directly within the storage/memory.
+               Do not take more than 32 bytes(256 bits).
+               int,uint,bool,address,byte,enum.
+
+
+2.Reference Type:- Holds address of the value location.
+                   Can take more than 32 bytes(256 bits).
+                   strings,structs,mapping and Arrays.        
+
+*/                
