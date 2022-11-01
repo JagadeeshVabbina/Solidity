705fe301aa96735bdb9108688b6aa12b04878135
JagadeeshVabbina <jagadeesh.vabbina@gmail.com>
2022-11-01 09:34:47 +0530

    StanleyKubrick,RobertSapolsky,AynRand,SheldonSolomon,SteveJobs

---
+++ Influences-1.sol
@@ -0,0 +1,205 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+contract ArjunJagadeesh {
+    bytes32 public mybytes32 = "ArjunJagadeesh";
+    struct MyStruct {
+        uint myUint;
+        string myString;
+    }
+
+    MyStruct public mystruct = MyStruct(28,"Arjun Jagadeesh Vabbina");
+}
+
+contract Arjun {
+    bytes32 public arjun = "Vabbina";
+    struct  onecrore {
+        string companyName;
+        uint salary;
+        uint time;
+        uint[] projects;
+    }
+    onecrore public Zero;
+    onecrore[] public equity;
+
+    function getValue() public {
+        Zero.companyName = "EthereumFoundation";
+        Zero.salary = 10000000;
+        Zero.time = 180;
+        Zero.projects.push(100);
+    }
+
+}
+
+contract crowdfunding {
+    bytes32 public caliber = "Neuroplasticity";
+
+    struct repetition {
+        string rajasekher;
+        uint myAge;
+        int myNetworth;
+        uint[] experiences;
+    }
+
+    repetition public Olympics;
+    repetition[] public rightist;
+
+    function setStruct() public {
+        Olympics.rajasekher = "Intermediate";
+        Olympics.myAge=28;
+        Olympics.myNetworth = -2500000;
+        Olympics.experiences.push(1000);
+    }
+}
+
+contract personality {
+    bytes32 public Years = "Twenty eight";
+
+    struct discipline {
+        string robertSapolsky;
+        uint Heros;
+        uint Obsession;
+        int[] debts;
+
+    }
+
+   discipline public Environment;
+   discipline[] public Epigenetics;
+
+   function setDicipline() public {
+       Environment.robertSapolsky="Do not Exarcebate";
+   } 
+    
+}
+
+// FirstDraft
+
+contract AynRand {
+    bytes32 public bookName = "TheFountainhead";
+
+    struct AtlasShrugged {
+        string heroName;
+        string Philosophy;
+        uint age;
+        string[] Ideas;
+    }
+
+    AtlasShrugged public Purpose;
+    AtlasShrugged[] public DagneyT;
+
+    function Reason() public {
+        Purpose.heroName = "John Golt";
+        Purpose.Philosophy = "Objectivism";
+        Purpose.age = 9;
+        Purpose.Ideas.push("SelfEsteem");
+    }
+}
+
+//SecondTake
+
+contract StanleyKubrick {
+    bytes32 public movieName = "2001: A Space Odyssy";
+    bool public Persuasion= true;
+    address public Dropout17 = 0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB;
+    string public ThoughtProcess = "Generic Problem Solving Ability";
+    uint public PhotographerJobyears = 4;
+
+    struct Group {
+        string FirstShortFilm;
+        string Absoluteness;
+        uint age;
+        string personality;
+        string[] movies;
+    } 
+Group public Filmmaking;
+Group[] public INTENSITY;
+
+function Perfectionism() public {
+    Filmmaking.FirstShortFilm = "Day of the Fight";
+    Filmmaking.Absoluteness = "Clarity,Caliber and Intelligence";
+    Filmmaking.age = 17;
+    Filmmaking.personality = "INTENSITY";
+    Filmmaking.movies.push("2001:A Space Odyssey");
+}
+
+}
+
+//ThirdScientificExperiment
+contract RobertSapolsky {
+    bool public FreeWill = false;
+    int public behavioursInfluencetime = -1000000000000000000000000000000000000000000000000000000000000000000;
+    uint public WhatShouldPayaRoleinLife = 777;//Forgiveness 
+    string public BrainRegion ="PreFrontal Cortex";
+    address public DoNotExacerbate = 0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C;
+    bytes32 public WhatOne_Should_have = "The Theory of Mind";
+
+    struct Empathy {
+        string AnteriorSingulate;
+        uint8 insularCortex;
+        bool Context_Density_Dependancy;
+        bytes32 Gratification_Postponement;
+        int8 Impulse_Control;
+        address USvsTHEM; 
+        string[] Stress;
+    }
+
+    Empathy public FREEWILL;
+    Empathy[] public Capacityto_Sin;
+
+    function Biology() public {
+        FREEWILL.AnteriorSingulate = "US,same color/hats/gods/family";
+        FREEWILL.insularCortex = 1 ;// disgust region of brain// for morals as well.
+        FREEWILL.Context_Density_Dependancy = true;
+        FREEWILL.Gratification_Postponement = "PFC matures at age 25";
+        FREEWILL.Impulse_Control = -28;
+        FREEWILL.USvsTHEM = 0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB;
+        FREEWILL.Stress.push("Chronic Stress ruins health");
+    }
+}
+
+// Fourth_TMT_Experiment
+contract SheldonSolomon {
+    string public bookName = " Worm at the Core";
+    bytes32 public Influencedby = "Ernest Becker";
+
+    struct DenialOfDeath {
+        uint Meaning_value;
+        int Death;
+        bool CultureValues;
+        string[] Heroism;
+    }
+
+    DenialOfDeath public FlightFromDeath;
+    DenialOfDeath[] public Immortality;
+
+    function Trivial_Grandeur() public {
+        FlightFromDeath.Meaning_value = 28;
+        FlightFromDeath.Death = -28;
+        FlightFromDeath.CultureValues = true;
+        FlightFromDeath.Heroism.push("Symbolic immortality");
+    }
+}
+
+//Fifth-Product
+contract SteveJobs {
+    bytes32 public CompanyName= "Apple Computers";
+    string public Personality = "Rebellious";
+    address public Adopted_dropOut = 0xdD870fA1b7C4700F2BD7f44238821C26f7392148;
+
+    struct Passion {
+        string RDF ;
+        uint age;
+        bool rejection;
+    }
+
+    Passion public DRIVEN;
+    Passion[] public Leadership;
+
+    function Decisiveness() public {
+        DRIVEN.RDF = "Cultivated";
+        DRIVEN.age = 19;
+        DRIVEN.rejection = true;
+    }
+}
+
+
+
