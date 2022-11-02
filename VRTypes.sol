a5b91cadd12c35830722e63ae86e9952ee032ad0
JagadeeshVabbina <jagadeesh.vabbina@gmail.com>
2022-11-02 10:30:16 +0530

    VR types..

---
+++ VRTypes.sol
@@ -0,0 +1,60 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.17;
+/// @title dataTypes in solidity programming language
+contract dataTypes {
+    /* 1. Value type:- Holds value directly within the storage/memory.
+      * Do not store more than 32 bytes(256 bits)
+      * int,uint,bool,address,byte,enum.
+
+      2.reference Type:- Holds the address of the location.
+      * can take more than 32 bytes(256 bits)
+      *strings,structs,Arrays(Fixed/Dynamic),mappings(Key/Value pair). */
+}
+
+// 2.
+
+contract ValueRef {
+    /* 1.valueType:- holds value directly within the storage/memory.
+    * can not take more than 32 bytes(256 bit).
+    * int,uint,bool,address,byte and enum.
+
+    2. Reference Type:- holds the address of the location where the value is stored.
+    * can take more than 32 bytes(256 bits)
+    * strings, structs, Arrays(fixed/dynamic) and Mapping(Key/Value pair). */
+}
+
+// 3.
+
+contract ValueRtypes{
+    /** 1. Value Type:- holds the value directly within storage/memory.
+    * can not take more than 32 bytes (256 bits).
+    * int,uint,bool,address,byte and enum.
+
+    * 2. Reference Types:- hold the address of the location.
+    * can take more than 32 bytes(256 bits)
+    * strings,structs,Arrays(fixed/dynamic) and Mapping(key/Value pair). */
+}
+
+// 4.
+contract VReferenceType {
+    /* 1. Value Type:- holds value directly within the storage/memory.
+    * can not take more than 32 bytes(256 bits)
+    * int,uint,bool,address,byte and enum.
+
+    2. reference Type:- holds the location where the value is stored.
+    * can take more than 32 bytes (256 bits).
+    * Structs,Strings, Arrays(fixed/dynamic) and Mapping(key/value pair). */
+}
+
+// 5.
+
+contract ValueReferenceTypes {
+    /* 1. value Type:- holds the value directly within the storage/memory.
+    * can not take more than 32 bytes (256 bits).
+    * int,uint,bool,address,bytes and enum.
+
+    2. Reference Type:- hold the location whewre the value is stored.
+    * Can take more than 32 bytes(256 bits).
+    * strings, structs, Arrays(fixed/dynamic) and mapping(key/value pair). */
+}
+
